4a95f8f898136d76fe16a6e31aa6974788c94a34
Fernando <fernandolasmar94@gmail.com>
2025-01-20 18:49:35 -0300

    first commit

---
+++ frontend/app/(main)/pages/user/page.tsx
@@ -0,0 +1,397 @@
+/* eslint-disable @next/next/no-img-element */
+'use client';
+import { Button } from 'primereact/button';
+import { Column } from 'primereact/column';
+import { DataTable } from 'primereact/datatable';
+import { Dialog } from 'primereact/dialog';
+import { FileUpload } from 'primereact/fileupload';
+import { InputNumber, InputNumberValueChangeEvent } from 'primereact/inputnumber';
+import { InputText } from 'primereact/inputtext';
+import { InputTextarea } from 'primereact/inputtextarea';
+import { RadioButton, RadioButtonChangeEvent } from 'primereact/radiobutton';
+import { Rating } from 'primereact/rating';
+import { Toast } from 'primereact/toast';
+import { Toolbar } from 'primereact/toolbar';
+import { classNames } from 'primereact/utils';
+import React, { useEffect, useRef, useState } from 'react';
+import { ProductService } from '../../../../demo/service/ProductService';
+import { Project } from '../../../../types/index'
+
+/* @todo Used 'as any' for types here. Will fix in next version due to onSelectionChange event type issue. */
+const Crud = () => {
+    let emptyUser: Project.User = {
+        id: '',
+        name: '',
+        login: '',
+        password: '',
+        email: ''
+    };
+
+    const [users, setUsers] = useState(null);
+    const [userDialog, setUserDialog] = useState(false);
+    const [deleteUserDialog, setDeleteUserDialog] = useState(false);
+    const [deleteUsersDialog, setDeleteUsersDialog] = useState(false);
+    const [user, setUser] = useState<Project.User>(emptyUser);
+    const [selectedUsers, setSelectedUsers] = useState(null);
+    const [submitted, setSubmitted] = useState(false);
+    const [globalFilter, setGlobalFilter] = useState('');
+    const toast = useRef<Toast>(null);
+    const dt = useRef<DataTable<any>>(null);
+
+    useEffect(() => {
+        // UserService.getUsers().then((data) => setUsers(data as any));
+    }, []);
+
+    const openNew = () => {
+        setUser(emptyUser);
+        setSubmitted(false);
+        setUserDialog(true);
+    };
+
+    const hideDialog = () => {
+        setSubmitted(false);
+        setUserDialog(false);
+    };
+
+    const hideDeleteUserDialog = () => {
+        setDeleteUserDialog(false);
+    };
+
+    const hideDeleteUsersDialog = () => {
+        setDeleteUsersDialog(false);
+    };
+
+    const saveUser = () => {
+        setSubmitted(true);
+
+        // if (user.name.trim()) {
+        //     let _users = [...(users as any)];
+        //     let _user = { ...user };
+        //     if (user.id) {
+        //         const index = findIndexById(user.id);
+
+        //         _users[index] = _user;
+        //         toast.current?.show({
+        //             severity: 'success',
+        //             summary: 'Successful',
+        //             detail: 'User Updated',
+        //             life: 3000
+        //         });
+        //     } else {
+        //         _user.id = createId();
+        //         _user.image = 'user-placeholder.svg';
+        //         _users.push(_user);
+        //         toast.current?.show({
+        //             severity: 'success',
+        //             summary: 'Successful',
+        //             detail: 'User Created',
+        //             life: 3000
+        //         });
+        //     }
+
+        //     setUsers(_users as any);
+        //     setUserDialog(false);
+        //     setUser(emptyUser);
+        // }
+    };
+
+    const editUser = (user: Project.User) => {
+        setUser({ ...user });
+        setUserDialog(true);
+    };
+
+    const confirmDeleteUser = (user: Project.User) => {
+        setUser(user);
+        setDeleteUserDialog(true);
+    };
+
+    const deleteUser = () => {
+        // let _users = (users as any)?.filter((val: any) => val.id !== user.id);
+        // setUsers(_users);
+        // setDeleteUserDialog(false);
+        // setUser(emptyUser);
+        // toast.current?.show({
+        //     severity: 'success',
+        //     summary: 'Successful',
+        //     detail: 'User Deleted',
+        //     life: 3000
+        // });
+    };
+
+    // const findIndexById = (id: string) => {
+    //     let index = -1;
+    //     for (let i = 0; i < (users as any)?.length; i++) {
+    //         if ((users as any)[i].id === id) {
+    //             index = i;
+    //             break;
+    //         }
+    //     }
+
+    //     return index;
+    // };
+
+    // const createId = () => {
+    //     let id = '';
+    //     let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    //     for (let i = 0; i < 5; i++) {
+    //         id += chars.charAt(Math.floor(Math.random() * chars.length));
+    //     }
+    //     return id;
+    // };
+
+    const exportCSV = () => {
+        dt.current?.exportCSV();
+    };
+
+    const confirmDeleteSelected = () => {
+        setDeleteUsersDialog(true);
+    };
+
+    const deleteSelectedUsers = () => {
+        let _users = (users as any)?.filter((val: any) => !(selectedUsers as any)?.includes(val));
+        setUsers(_users);
+        setDeleteUsersDialog(false);
+        setSelectedUsers(null);
+        toast.current?.show({
+            severity: 'success',
+            summary: 'Successful',
+            detail: 'Usuários deletados',
+            life: 3000
+        });
+    };
+
+    // const onCategoryChange = (e: RadioButtonChangeEvent) => {
+    //     let _user = { ...user };
+    //     _user['category'] = e.value;
+    //     setUser(_user);
+    // };
+
+    const onInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, name: string) => {
+        const val = (e.target && e.target.value) || '';
+        let _user = { ...user };
+        _user[`${name}`] = val;
+
+        setUser(_user);
+    };
+
+    // const onInputNumberChange = (e: InputNumberValueChangeEvent, name: string) => {
+    //     const val = e.value || 0;
+    //     let _user = { ...user };
+    //     _user[`${name}`] = val;
+
+    //     setUser(_user);
+    // };
+
+    const leftToolbarTemplate = () => {
+        return (
+            <React.Fragment>
+                <div className="my-2">
+                    <Button label="Novo" icon="pi pi-plus" severity="success" className=" mr-2" onClick={openNew} />
+                    <Button label="Excluir" icon="pi pi-trash" severity="danger" onClick={confirmDeleteSelected} disabled={!selectedUsers || !(selectedUsers as any).length} />
+                </div>
+            </React.Fragment>
+        );
+    };
+
+    const rightToolbarTemplate = () => {
+        return (
+            <React.Fragment>
+                <FileUpload mode="basic" accept="image/*" maxFileSize={1000000} chooseLabel="Import" className="mr-2 inline-block" />
+                <Button label="Export" icon="pi pi-upload" severity="help" onClick={exportCSV} />
+            </React.Fragment>
+        );
+    };
+
+    const idBodyTemplate = (rowData: Project.User) => {
+        return (
+            <>
+                <span className="p-column-title">Código</span>
+                {rowData.id}
+            </>
+        );
+    };
+
+    const nameBodyTemplate = (rowData: Project.User) => {
+        return (
+            <>
+                <span className="p-column-title">Nome</span>
+                {rowData.name}
+            </>
+        );
+    };
+
+    const loginBodyTemplate = (rowData: Project.User) => {
+        return (
+            <>
+                <span className="p-column-title">Login</span>
+                {rowData.login}
+            </>
+        );
+    };
+
+    const emailBodyTemplate = (rowData: Project.User) => {
+        return (
+            <>
+                <span className="p-column-title">Email</span>
+                {rowData.email}
+            </>
+        );
+    };
+
+    const actionBodyTemplate = (rowData: Project.User) => {
+        return (
+            <>
+                <Button icon="pi pi-pencil" rounded severity="success" className="mr-2" onClick={() => editUser(rowData)} />
+                <Button icon="pi pi-trash" rounded severity="warning" onClick={() => confirmDeleteUser(rowData)} />
+            </>
+        );
+    };
+
+    const header = (
+        <div className="flex flex-column md:flex-row md:justify-content-between md:align-items-center">
+            <h5 className="m-0">Gerenciamento de Usuários</h5>
+            <span className="block mt-2 md:mt-0 p-input-icon-left">
+                <i className="pi pi-search" />
+                <InputText type="search" onInput={(e) => setGlobalFilter(e.currentTarget.value)} placeholder="Search..." />
+            </span>
+        </div>
+    );
+
+    const userDialogFooter = (
+        <>
+            <Button label="Cancelar" icon="pi pi-times" text onClick={hideDialog} />
+            <Button label="Salvar" icon="pi pi-check" text onClick={saveUser} />
+        </>
+    );
+    const deleteUserDialogFooter = (
+        <>
+            <Button label="Não" icon="pi pi-times" text onClick={hideDeleteUserDialog} />
+            <Button label="Sim" icon="pi pi-check" text onClick={deleteUser} />
+        </>
+    );
+    const deleteUsersDialogFooter = (
+        <>
+            <Button label="Não" icon="pi pi-times" text onClick={hideDeleteUsersDialog} />
+            <Button label="Sim" icon="pi pi-check" text onClick={deleteSelectedUsers} />
+        </>
+    );
+
+    return (
+        <div className="grid crud-demo">
+            <div className="col-12">
+                <div className="card">
+                    <Toast ref={toast} />
+                    <Toolbar className="mb-4" left={leftToolbarTemplate} right={rightToolbarTemplate}></Toolbar>
+
+                    <DataTable
+                        ref={dt}
+                        value={users}
+                        selection={selectedUsers}
+                        onSelectionChange={(e) => setSelectedUsers(e.value as any)}
+                        dataKey="id"
+                        paginator
+                        rows={10}
+                        rowsPerPageOptions={[5, 10, 25]}
+                        className="datatable-responsive"
+                        paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+                        currentPageReportTemplate="Mostrando {first} até {last} de {totalRecords} usuários"
+                        globalFilter={globalFilter}
+                        emptyMessage="Nenhum usuário encontrado."
+                        header={header}
+                        responsiveLayout="scroll"
+                    >
+                        <Column selectionMode="multiple" headerStyle={{ width: '4rem' }}></Column>
+                        <Column field="id" header="Código" sortable body={idBodyTemplate} headerStyle={{ minWidth: '15rem' }}></Column>
+                        <Column field="name" header="Nome" sortable body={nameBodyTemplate} headerStyle={{ minWidth: '15rem' }}></Column>
+                        <Column field="login" header="Login" sortable body={loginBodyTemplate} headerStyle={{ minWidth: '15rem' }}></Column>
+                        <Column field="email" header="Email" sortable body={emailBodyTemplate} headerStyle={{ minWidth: '15rem' }}></Column>
+                        <Column body={actionBodyTemplate} headerStyle={{ minWidth: '10rem' }}></Column>
+                    </DataTable>
+
+                    <Dialog visible={userDialog} style={{ width: '450px' }} header="Detalhes do Usuário" modal className="p-fluid" footer={userDialogFooter} onHide={hideDialog}>
+                        <div className="field">
+                            <label htmlFor="name">Nome</label>
+                            <InputText
+                                id="name"
+                                value={user.name}
+                                onChange={(e) => onInputChange(e, 'name')}
+                                required
+                                autoFocus
+                                className={classNames({
+                                    'p-invalid': submitted && !user.name
+                                })}
+                            />
+                            {submitted && !user.name && <small className="p-invalid">Nome é obrigatório.</small>}
+                        </div>
+
+                        <div className="field">
+                            <label htmlFor="login">Login</label>
+                            <InputText
+                                id="login"
+                                value={user.login}
+                                onChange={(e) => onInputChange(e, 'login')}
+                                required
+                                autoFocus
+                                className={classNames({
+                                    'p-invalid': submitted && !user.login
+                                })}
+                            />
+                            {submitted && !user.login && <small className="p-invalid">Login é obrigatório.</small>}
+                        </div>
+
+                        <div className="field">
+                            <label htmlFor="password">Password</label>
+                            <InputText
+                                id="password"
+                                value={user.password}
+                                onChange={(e) => onInputChange(e, 'password')}
+                                required
+                                autoFocus
+                                className={classNames({
+                                    'p-invalid': submitted && !user.password
+                                })}
+                            />
+                            {submitted && !user.password && <small className="p-invalid">Senha é obrigatória.</small>}
+                        </div>
+
+                        <div className="field">
+                            <label htmlFor="email">Email</label>
+                            <InputText
+                                id="email"
+                                value={user.login}
+                                onChange={(e) => onInputChange(e, 'email')}
+                                required
+                                autoFocus
+                                className={classNames({
+                                    'p-invalid': submitted && !user.email
+                                })}
+                            />
+                            {submitted && !user.email && <small className="p-invalid">Email é obrigatório.</small>}
+                        </div>
+
+                    </Dialog>
+
+                    <Dialog visible={deleteUserDialog} style={{ width: '450px' }} header="Confirmar" modal footer={deleteUserDialogFooter} onHide={hideDeleteUserDialog}>
+                        <div className="flex align-items-center justify-content-center">
+                            <i className="pi pi-exclamation-triangle mr-3" style={{ fontSize: '2rem' }} />
+                            {user && (
+                                <span>
+                                    Você realmente deseja excluir o usuário<b>{user.name}</b>?
+                                </span>
+                            )}
+                        </div>
+                    </Dialog>
+
+                    <Dialog visible={deleteUsersDialog} style={{ width: '450px' }} header="Confirmar" modal footer={deleteUsersDialogFooter} onHide={hideDeleteUsersDialog}>
+                        <div className="flex align-items-center justify-content-center">
+                            <i className="pi pi-exclamation-triangle mr-3" style={{ fontSize: '2rem' }} />
+                            {user && <span>Você realmente desejar excluir os usuários selecionados?</span>}
+                        </div>
+                    </Dialog>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Crud;
